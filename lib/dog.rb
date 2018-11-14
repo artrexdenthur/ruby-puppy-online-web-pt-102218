77af18f5b507eaec9f4a58a8ce64653630d25319
Paul Ashour <paul.ashour@gmail.com>
2018-11-14 22:04:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,20 @@
 class Dog
   @@all = []
+  attr_accessor :name
   
   def self.clear_all()
     @@all = []
   end
   
+  def self.all()
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
   
+  def self.new(name)
+    @name = name
+    @@all << self
+  end
   
 end
